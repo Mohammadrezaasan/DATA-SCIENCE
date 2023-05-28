8b64cf35c93231b9aec75764234d8e016e26370e
Mohammad Reza Asan <mohammadrezaasan7@gmail.com>
2023-05-28 16:19:06 +0330

    Add files via upload

--- Exercises/python_one_exercises/q_1.py
+++ Exercises/python_one_exercises/q_1.py
@@ -1,1 +1,11 @@
-
+def table() :
+    text = (input(str('Enter your text : ')).replace(' ',''))
+    result = []
+    text = list(text)
+    for x in text :result.append(f'|   {x}   |     '+str(text.count(x)).center(5)+'     |')
+    result = set(result)
+    final_text = "+-------+"+"---------------+\n|  NAME |"+"   FREQUENCY   |\n+-------+"+"---------------+\n"
+    for x in result :final_text += str(x)+'\n'
+    final_text += '+-------+'+'---------------+'
+    print(final_text)
+table()
